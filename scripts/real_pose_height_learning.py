3b0e732399c95421981796a8f7ede0f64a4f60a3
98shimpei <satoshin.980608@gmail.com>
2022-09-16 18:26:52 +0900

    add viewer, update checkpoint

--- scripts/real_pose_height_learning.py
+++ scripts/real_pose_height_learning.py
@@ -19,9 +19,18 @@ if len(args) == 3:
     args.append("fwv")
 print(args)
 
+terrains_x = []
+terrains_y = []
+pose_terrains = []
 surfaces = []
+for filename in os.listdir("../terrains/x"):
+    terrains_x.append(np.loadtxt("../terrains/x/"+filename, delimiter=","))
+    terrains_y.append(np.loadtxt("../terrains/y/"+filename, delimiter=","))
 for filename in os.listdir("../surfaces"):
     surfaces.append(np.loadtxt("../surfaces/"+filename, delimiter=","))
+steppable_terrains = [terrains_x[i] for i in range(len(terrains_y)) if np.sum(terrains_y[i])==25]
+for filename in os.listdir("../terrains/pose"):
+    steppable_terrains.append(np.loadtxt("../terrains/pose/"+filename, delimiter=","))
 
 #----------------------------
 # データの作成
@@ -44,48 +53,69 @@ def randomize(x, y):
 
 def generate_data(num):
     x_data = np.zeros((num, H, W, C))
-    y_data = np.ones((num, 1, 1, 3))
+    y_data = np.zeros((num, 1, 1, 3))
     for i in range(num):
         surface_index = math.floor(random.random()*len(surfaces))
         surface_x = math.floor(random.random() * (surfaces[surface_index].shape[1] - 21))
         surface_y = math.floor(random.random() * (surfaces[surface_index].shape[0] - 21))
         x_data[i, :, :, 0] += surfaces[surface_index][surface_y:surface_y+21, surface_x:surface_x+21]
+        
+        #terrain
+        x_data[i, :, :, 0] += steppable_terrains[math.floor(random.random()*len(steppable_terrains))][13:34,13:34]
 
+        for j in range(2):
+            h = random.random()*0.3
+            l = math.floor(random.random()*8+1)
+            tmp = random.random()
+            if tmp < 0.1: #端を消す
+                x_data[i, 0:l, :, 0] -= h * np.ones((l, x_data.shape[2]))
+            elif tmp < 0.2:
+                x_data[i, x_data.shape[1]-l:x_data.shape[1], :, 0] -= h * np.ones((l, x_data.shape[2]))
+            elif tmp < 0.3:
+                x_data[i, :, 0:l, 0] -= h * np.ones((x_data.shape[1], l))
+            elif tmp < 0.4:
+                x_data[i, :, x_data.shape[2]-l:x_data.shape[2], 0] -= h * np.ones((x_data.shape[1], l))
+
+        #rotate
+        theta = random.random() * 360
+        M = cv2.getRotationMatrix2D((math.floor(x_data.shape[2] / 2.), math.floor(x_data.shape[1] / 2.)), theta, 1)
+        x_data[i, :, :, 0] = cv2.warpAffine(x_data[i], M, (x_data.shape[2], x_data.shape[1]))
+
+        #tilt
         p = 2.0*random.random() - 1.0
         r = 2.0*random.random() - 1.0
         s = 1.0*random.random() - 0.5
         x_data[i] += p * pitch + r * roll + s * scale + 0.05*np.random.rand(H, W, 1) - 0.025 * np.ones((H, W, 1))
         y_data[i, 0, 0] = np.array([p, r, s])
-
-    for i in range(math.floor(num*0.8)):
-        n = math.floor(7 * random.random())
-        for j in range(n):
-            l = math.floor(5 * random.random()) + 2
-            x = math.floor((W-l) * random.random())
-            y = math.floor((H-l) * random.random())
-            h = 0.5 * random.random()
-            x_data[i, x:x+l, y:y+l] -= h * np.ones((l, l, 1))
-            pp = 1.0 * random.random()
-            qq = 1.0 * random.random()
-            for p in range(l):
-                for q in range(l):
-                    x_data[i, x+p, y+q, 0] -= (p * pp + q * qq) * 0.01
-    x_data, y_data = randomize(x_data, y_data)
-    for i in range(math.floor(num*0.1)):
-        l = math.floor(random.random()*6)+1
-        x_data[i, 0 : l, :, 0] -= np.ones((l, x_data.shape[2])) * random.random() * 0.4
-    x_data, y_data = randomize(x_data, y_data)
-    for i in range(math.floor(num*0.1)):
-        l = math.floor(random.random()*6)+1
-        x_data[i, x_data.shape[1]-l : x_data.shape[1], :, 0] -= np.ones((l, x_data.shape[2])) * random.random() * 0.4
-    x_data, y_data = randomize(x_data, y_data)
-    for i in range(math.floor(num*0.1)):
-        l = math.floor(random.random()*6)+1
-        x_data[i, :, 0 : l, 0] -= np.ones((x_data.shape[1], l)) * random.random() * 0.4
-    x_data, y_data = randomize(x_data, y_data)
-    for i in range(math.floor(num*0.1)):
-        l = math.floor(random.random()*6)+1
-        x_data[i, :, x_data.shape[2]-l : x_data.shape[2], 0] -= np.ones((x_data.shape[1], l)) * random.random() * 0.4
+    #for i in range(math.floor(num*0.8)):
+    #    n = math.floor(7 * random.random())
+    #    for j in range(n):
+    #        l = math.floor(5 * random.random()) + 2
+    #        x = math.floor((W-l) * random.random())
+    #        y = math.floor((H-l) * random.random())
+    #        h = 0.5 * random.random()
+    #        x_data[i, x:x+l, y:y+l] -= h * np.ones((l, l, 1))
+    #        pp = 1.0 * random.random()
+    #        qq = 1.0 * random.random()
+    #        for p in range(l):
+    #            for q in range(l):
+    #                x_data[i, x+p, y+q, 0] -= (p * pp + q * qq) * 0.01
+    #x_data, y_data = randomize(x_data, y_data)
+    #for i in range(math.floor(num*0.1)):
+    #    l = math.floor(random.random()*6)+1
+    #    x_data[i, 0 : l, :, 0] -= np.ones((l, x_data.shape[2])) * random.random() * 0.4
+    #x_data, y_data = randomize(x_data, y_data)
+    #for i in range(math.floor(num*0.1)):
+    #    l = math.floor(random.random()*6)+1
+    #    x_data[i, x_data.shape[1]-l : x_data.shape[1], :, 0] -= np.ones((l, x_data.shape[2])) * random.random() * 0.4
+    #x_data, y_data = randomize(x_data, y_data)
+    #for i in range(math.floor(num*0.1)):
+    #    l = math.floor(random.random()*6)+1
+    #    x_data[i, :, 0 : l, 0] -= np.ones((x_data.shape[1], l)) * random.random() * 0.4
+    #x_data, y_data = randomize(x_data, y_data)
+    #for i in range(math.floor(num*0.1)):
+    #    l = math.floor(random.random()*6)+1
+    #    x_data[i, :, x_data.shape[2]-l : x_data.shape[2], 0] -= np.ones((x_data.shape[1], l)) * random.random() * 0.4
 
     return x_data, y_data
 
@@ -143,16 +173,20 @@ if "f" in args[3]:
 
 if "v" in args[3]:
     cv2.imshow('test',x_test[0] * 1.0 + 0.5)
-
-    print("answer")
-    print(y_train[70])
-    print("predict")
-    print(model_pose.predict(x_train[70:71]))
-    print(model_height.predict(x_train[70:71]))
-    cv2.imshow('test',x_train[70] * 1.0 + 0.5)
     cv2.waitKey(1)
-    time.sleep(5)
-    print("---")
+
+    print(len(steppable_terrains))
+
+    for i in range(5):
+        print("answer")
+        print(y_train[i])
+        print("predict")
+        print(model_pose.predict(x_train[i:i+1]))
+        print(model_height.predict(x_train[i:i+1]))
+        cv2.imshow('test',x_train[i] * 1.0 + 0.5)
+        cv2.waitKey(1)
+        time.sleep(5)
+        print("---")
 
     print("answer")
     print(y_test[70])
--- scripts/save_surface.py
+++ scripts/save_surface.py
@@ -11,8 +11,11 @@ import rospy
 import roslib
 import ros_numpy
 import datetime
+import copy
 from sensor_msgs.msg import Image
-#import cnn_models
+import cnn_models
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
 
 args = sys.argv
 if len(args) == 1:
@@ -21,7 +24,8 @@ print(args)
 
 class SurfaceSaver:
     def __init__(self):
-        self.drawing = False
+        self.drag = False
+        self.calc_pose = False
         self.mouse_x1 = -1
         self.mouse_y1 = -1
         self.mouse_x2 = -1
@@ -29,32 +33,83 @@ class SurfaceSaver:
         self.color = 0
         self.sum_image = np.zeros((47, 47))
         self.sum_counter = 0
+        self.model_steppable = cnn_models.cnn_steppable((37, 37, 1), "../checkpoints/checkpoint")
+        self.model_height = cnn_models.cnn_height((21, 21, 1), "../checkpoints/checkpoint")
+        self.model_pose = cnn_models.cnn_pose((21, 21, 1), "../checkpoints/checkpoint")
+
+        self.marker_publisher = rospy.Publisher('landing_pose_marker', Marker, queue_size = 1)
         rospy.Subscriber('rt_filtered_current_heightmap/output', Image, self.surfaceSaver, queue_size=1, buff_size=2**24)
 
     def mousePoints(self, event, x, y, flags, params):
         if event == cv2.EVENT_LBUTTONDOWN:
             self.mouse_x1 = self.mouse_x2 = math.floor(x/2.0)
-            self.mouse_y1 = self.mouse_y2 = math.floor(y/2.0)
-            self.drawing = True
+            self.mouse_y1 = self.mouse_y2 = math.floor(500-y/2.0)
+            self.drag = True
+            self.calc_pose = True
         elif event == cv2.EVENT_MOUSEMOVE:
-            if self.drawing:
+            if self.drag:
                 self.mouse_x2 = math.floor(x/2.0)
-                self.mouse_y2 = math.floor(y/2.0)
+                self.mouse_y2 = math.floor(500-y/2.0)
         elif event == cv2.EVENT_LBUTTONUP:
             self.mouse_x2 = math.floor(x/2.0)
-            self.mouse_y2 = math.floor(y/2.0)
-            self.drawing = False
+            self.mouse_y2 = math.floor(500-y/2.0)
+            self.drag = False
+
+    def publishPoseMsg(self, header, action, start_pos, ez, is_steppable):
+        end_pos = start_pos + 0.3 * ez
+        pose_msg = Marker()
+        pose_msg.header = copy.deepcopy(header)
+        pose_msg.header.stamp = rospy.Time.now()
+        pose_msg.ns = "recognized_pose"
+        pose_msg.id = 0
+        pose_msg.type = Marker.ARROW
+        pose_msg.action = action
+        start = Point()
+        start.x = start_pos[0]
+        start.y = start_pos[1]
+        start.z = start_pos[2]
+        end = Point()
+        end.x = end_pos[0]
+        end.y = end_pos[1]
+        end.z = end_pos[2]
+        pose_msg.points.append(start)
+        pose_msg.points.append(end)
+        if is_steppable:
+            pose_msg.color.r = 0.2
+            pose_msg.color.g = 1.0
+            pose_msg.color.b = 0.2
+        else:
+            pose_msg.color.r = 1.0
+            pose_msg.color.g = 0.2
+            pose_msg.color.b = 0.2
+        pose_msg.color.a = 1.0
+        pose_msg.scale.x = 0.03
+        pose_msg.scale.y = 0.05
+        pose_msg.scale.z = 0.07
+        pose_msg.pose.orientation.w = 1.0
+        self.marker_publisher.publish(pose_msg)
 
     def surfaceSaver(self, msg):
         input_image = ros_numpy.numpify(msg)
         input_image = input_image[:, :, 0]
         input_image = input_image - (input_image<-1e5)*input_image + (input_image<-1e5) * cv2.blur(input_image - (input_image<-1e5)*input_image, (9, 9)) / cv2.blur((input_image>-1e5).astype(float), (9, 9))
         np.nan_to_num(input_image, copy=False, nan=-1e10)
-        vision_image = input_image.copy()
+        vision_image = input_image.copy() * 1.2
         vision_image = np.where(vision_image > 0.5, 0.5, vision_image)
         vision_image = np.where(vision_image < -0.5, -0.5, vision_image)
         vision_image = vision_image + 0.5
         while True:
+            if self.calc_pose:
+                self.calc_pose = False
+                predict_image = input_image.copy()
+                predict_image = predict_image.reshape((1,input_image.shape[0],input_image.shape[1],1))
+                is_steppable = np.argmax(self.model_steppable.predict(predict_image[:,self.mouse_y1-18:self.mouse_y1+19, self.mouse_x1-18:self.mouse_x1+19, :]))
+                height = self.model_height.predict(predict_image[:, self.mouse_y1-10:self.mouse_y1+11, self.mouse_x1-10:self.mouse_x1+11, :])[0,0,0,0]
+                pose = self.model_pose.predict(predict_image[:, self.mouse_y1-10:self.mouse_y1+11, self.mouse_x1-10:self.mouse_x1+11, :])[0,0,0]
+                tmp_vecx = [1.0, 0.0, pose[0]]
+                tmp_vecy = [0.0, 1.0, pose[1]]
+                tmp_vecz = np.cross(tmp_vecx, tmp_vecy)
+                tmp_vecz = tmp_vecz / np.linalg.norm(tmp_vecz)
             tmp_image = vision_image.copy()
             self.color += 0.014
             if self.color > 2:
@@ -65,17 +120,20 @@ class SurfaceSaver:
                 #cv2.circle(tmp_image, (self.mouse_x1, self.mouse_y1), 2, math.fabs(self.color-1), thickness=-1)
                 cv2.rectangle(tmp_image, (self.mouse_x1-1, self.mouse_y1-1), (self.mouse_x2+1, self.mouse_y2+1), math.fabs(self.color - 1), 1)
                 cv2.rectangle(tmp_image, (self.mouse_x1-11, self.mouse_y1-11), (self.mouse_x2+11, self.mouse_y2+11), math.fabs(self.color - 1), 1)
-                cv2.rectangle(tmp_image, (self.mouse_x1-20, self.mouse_y1-20), (self.mouse_x2+20, self.mouse_y2+20), math.fabs(self.color - 1), 1)
+                cv2.rectangle(tmp_image, (self.mouse_x1-19, self.mouse_y1-19), (self.mouse_x2+19, self.mouse_y2+19), math.fabs(self.color - 1), 1)
                 cv2.rectangle(tmp_image, (self.mouse_x1-23, self.mouse_y1-23), (self.mouse_x2+23, self.mouse_y2+23), math.fabs(self.color - 1), 1)
             else: #surface
                 cv2.rectangle(tmp_image, (self.mouse_x1, self.mouse_y1), (self.mouse_x2, self.mouse_y2), math.fabs(self.color - 1), 2)
             tmp_image = cv2.resize(tmp_image, (tmp_image.shape[1]*2, tmp_image.shape[0]*2))
+            tmp_image = cv2.flip(tmp_image, 0)
             cv2.imshow("surface_saver", tmp_image)
             cv2.setMouseCallback("surface_saver", self.mousePoints)
 
             key = cv2.waitKey(10)
             if key == 46 or key == 8 or key == 255: #delete, backspace
                 self.mouse_x1 = self.mouse_x2 = self.mouse_y1 = self.mouse_y2 = -1
+                if not self.calc_pose:
+                    self.publishPoseMsg(msg.header, Marker.DELETE, np.array([(self.mouse_x1)*0.01, (self.mouse_y1-250)*0.01, height]), tmp_vecz, is_steppable)
                 print("cancel")
                 break
 
@@ -83,6 +141,9 @@ class SurfaceSaver:
                 pass
             elif self.mouse_x1 == self.mouse_x2 and self.mouse_y1 == self.mouse_y2: #terrain
                 save_image = input_image[self.mouse_y1-23:self.mouse_y2+24, self.mouse_x1-23:self.mouse_x2+24]
+                if not self.calc_pose:
+                    self.publishPoseMsg(msg.header, Marker.ADD, np.array([(self.mouse_x1)*0.01, (self.mouse_y1-250)*0.01, height]), tmp_vecz, is_steppable)
+
                 if key == 111: #O
                     nowdate = datetime.datetime.now()
                     np.savetxt("../terrains/x/"+args[1]+nowdate.strftime('%y%m%d_%H%M%S')+".csv", save_image, delimiter=",")
@@ -97,7 +158,15 @@ class SurfaceSaver:
                     self.mouse_x1 = self.mouse_x2 = self.mouse_y1 = self.mouse_y2 = -1
                     print("save steppable image")
                     break
+                elif key == 112: #P
+                    nowdate = datetime.datetime.now()
+                    np.savetxt("../terrains/pose/"+args[1]+nowdate.strftime('%y%m%d_%H%M%S')+".csv", save_image, delimiter=",")
+                    self.mouse_x1 = self.mouse_x2 = self.mouse_y1 = self.mouse_y2 = -1
+                    print("save steppable image")
+                    break
             else: #surfece
+                if not self.calc_pose:
+                    self.publishPoseMsg(msg.header, Marker.DELETE, np.array([(self.mouse_x1)*0.01, (self.mouse_y1-250)*0.01, height]), tmp_vecz, is_steppable)
                 if key == 13: #Enter
                     if self.mouse_x1 > self.mouse_x2:
                         tmp = self.mouse_x1
